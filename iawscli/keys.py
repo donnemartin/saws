8c2a390e0d8e8779ed4b7eec4a40c2fe7d04448f
Donne Martin <donne.martin@gmail.com>
2015-08-29 09:37:52 -0400

    Reworked keys to support command like function keys.

--- iawscli/keys.py
+++ iawscli/keys.py
@@ -1,100 +1,70 @@
+# -*- coding: utf-8
 from __future__ import unicode_literals
+from __future__ import print_function
 
-__all__ = (
-    'Key',
-    'Keys',
-)
+import webbrowser
 
-
-class Key(object):
-    def __init__(self, name):
-
-        #: Descriptive way of writing keys in configuration files. e.g. <C-A>
-        #: for ``Control-A``.
-        self.name = name
-
-    def __repr__(self):
-        return '%s(%r)' % (self.__class__.__name__, self.name)
+from prompt_toolkit.key_binding.manager import KeyBindingManager
+from prompt_toolkit.keys import Keys
 
 
-class Keys(object):
-    Escape = Key('<Escape>')
+def get_key_manager(set_fuzzy_match, get_fuzzy_match,
+                    refresh_resources, handle_docs):
+    """
+    Create and initialize keybinding manager
+    :return: KeyBindingManager
+    """
 
-    ControlA = Key('<C-A>')
-    ControlB = Key('<C-B>')
-    ControlC = Key('<C-C>')
-    ControlD = Key('<C-D>')
-    ControlE = Key('<C-E>')
-    ControlF = Key('<C-F>')
-    ControlG = Key('<C-G>')
-    ControlH = Key('<C-H>')
-    ControlI = Key('<C-I>')  # Tab
-    ControlJ = Key('<C-J>')  # Enter
-    ControlK = Key('<C-K>')
-    ControlL = Key('<C-L>')
-    ControlM = Key('<C-M>')  # Enter
-    ControlN = Key('<C-N>')
-    ControlO = Key('<C-O>')
-    ControlP = Key('<C-P>')
-    ControlQ = Key('<C-Q>')
-    ControlR = Key('<C-R>')
-    ControlS = Key('<C-S>')
-    ControlT = Key('<C-T>')
-    ControlU = Key('<C-U>')
-    ControlV = Key('<C-V>')
-    ControlW = Key('<C-W>')
-    ControlX = Key('<C-X>')
-    ControlY = Key('<C-Y>')
-    ControlZ = Key('<C-Z>')
+    assert callable(set_fuzzy_match)
+    assert callable(get_fuzzy_match)
+    assert callable(refresh_resources)
+    assert callable(handle_docs)
 
-    ControlSpace       = Key('<C-Space>')
-    ControlBackslash   = Key('<C-Backslash>')
-    ControlSquareClose = Key('<C-SquareClose>')
-    ControlCircumflex  = Key('<C-Circumflex>')
-    ControlUnderscore  = Key('<C-Underscore>')
-    ControlLeft        = Key('<C-Left>')
-    ControlRight       = Key('<C-Right>')
-    ControlUp          = Key('<C-Up>')
-    ControlDown        = Key('<C-Down>')
+    manager = KeyBindingManager(enable_system_bindings=True)
 
-    Up          = Key('<Up>')
-    Down        = Key('<Down>')
-    Right       = Key('<Right>')
-    Left        = Key('<Left>')
-    Home        = Key('<Home>')
-    End         = Key('<End>')
-    Delete      = Key('<Delete>')
-    ShiftDelete = Key('<ShiftDelete>')
-    PageUp      = Key('<PageUp>')
-    PageDown    = Key('<PageDown>')
-    BackTab     = Key('<BackTab>')  # shift + tab
+    @manager.registry.add_binding(Keys.F2)
+    def _(event):
+        """
+        When F2 has been pressed, fill in the "docs" command.
+        """
+        handle_docs()
 
-    Tab         = ControlI
-    Backspace   = ControlH
+    @manager.registry.add_binding(Keys.F4)
+    def _(_):
+        """
+        Enable/Disable fuzzy matching.
+        """
+        set_fuzzy_match(not get_fuzzy_match())
 
-    F1 = Key('<F1>')
-    F2 = Key('<F2>')
-    F3 = Key('<F3>')
-    F4 = Key('<F4>')
-    F5 = Key('<F5>')
-    F6 = Key('<F6>')
-    F7 = Key('<F7>')
-    F8 = Key('<F8>')
-    F9 = Key('<F9>')
-    F10 = Key('<F10>')
-    F11 = Key('<F11>')
-    F12 = Key('<F12>')
-    F13 = Key('<F13>')
-    F14 = Key('<F14>')
-    F15 = Key('<F15>')
-    F16 = Key('<F16>')
-    F17 = Key('<F17>')
-    F18 = Key('<F18>')
-    F19 = Key('<F19>')
-    F20 = Key('<F20>')
+    @manager.registry.add_binding(Keys.F5)
+    def _(_):
+        """
+        Refreshes AWS resources.
+        """
+        refresh_resources()
 
-    # Matches any key.
-    Any = Key('<Any>')
+    @manager.registry.add_binding(Keys.F10)
+    def _(event):
+        """
+        When F10 has been pressed, quit.
+        """
+        # Unused parameters for linter.
+        raise EOFError
 
-    # Special
-    CPRResponse = Key('<Cursor-Position-Response>')
+    @manager.registry.add_binding(Keys.ControlSpace)
+    def _(event):
+        """
+        Initialize autocompletion at cursor.
+
+        If the autocompletion menu is not showing, display it with the
+        appropriate completions for the context.
+
+        If the menu is showing, select the next completion.
+        """
+        b = event.cli.current_buffer
+        if b.complete_state:
+            b.complete_next()
+        else:
+            event.cli.start_completion(select_first=False)
+
+    return manager
