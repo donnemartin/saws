81dc14eee35528c441434161cd53a55840ee1106
Donne Martin <donne.martin@gmail.com>
2015-08-31 06:29:22 -0400

    Tweak lexer.

--- iawscli/lexer.py
+++ iawscli/lexer.py
@@ -1,30 +1,26 @@
 # -*- coding: utf-8
 from pygments.lexer import RegexLexer
 from pygments.lexer import words
-from pygments.token import Operator, Keyword, Text, Name
+from pygments.token import Keyword, Name, Operator, Generic, Literal
 from .commands import GLOBAL_OPTIONS, RESOURCE_OPTIONS, \
     AWS_COMMAND, AWS_DOCS, all_commands
 
 
 class CommandLexer(RegexLexer):
-    name = 'Command Line'
-    aliases = ['cli']
-    filenames = []
     commands = all_commands()
     tokens = {
         'root': [
             (words(tuple(AWS_COMMAND), prefix=r'', suffix=r'\b'),
+             Literal.String),
+            (words(tuple(commands[0]), prefix=r'', suffix=r'\b'),
+             Name.Class),
+            (words(tuple(commands[1]), prefix=r'', suffix=r'\b'),
+             Keyword.Declaration),
+            (words(tuple(GLOBAL_OPTIONS), prefix=r'', suffix=r'\b'),
+             Generic.Output),
+            (words(tuple(RESOURCE_OPTIONS), prefix=r'', suffix=r'\b'),
              Operator.Word),
             (words(tuple(AWS_DOCS), prefix=r'', suffix=r'\b'),
-             Operator.Word),
-            (words(tuple(commands[0]), prefix=r'', suffix=r'\b'),
-             Operator.Word),
-            (words(tuple(commands[1]), prefix=r'', suffix=r'\b'),
-             Operator.Word),
-            (words(tuple(GLOBAL_OPTIONS), prefix=r'', suffix=r'\b'),
-             Keyword),
-            (words(tuple(RESOURCE_OPTIONS), prefix=r'', suffix=r'\b'),
-             Keyword),
-            (r'.*\n', Text),
+             Literal.Number),
         ]
     }
