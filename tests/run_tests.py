829cf70d4788b4cdb6ae9de47ac4842a890ca3d0
Donne Martin <donne.martin@gmail.com>
2015-09-03 07:37:10 -0400

    Added run_tests.py, which runs other unit tests.

---
+++ tests/run_tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+import unittest
+from test_completer import CompleterTest
+from test_commands import CommandsTest
+
+
+if __name__ == '__main__':
+    unittest.main()
