15227015df38b2d76761a8260e25b9ba8efdc75b
Donne Martin <donne.martin@gmail.com>
2015-09-04 07:23:31 -0400

    Supress unused import lint check.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 import unittest
-from test_completer import CompleterTest
-from test_commands import CommandsTest
+from test_completer import CompleterTest  # NOQA
+from test_commands import CommandsTest  # NOQA
 
 
 if __name__ == '__main__':
