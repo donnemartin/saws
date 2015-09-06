64eed7867a518cb0dd4b28b8929a8114de71b8a9
Donne Martin <donne.martin@gmail.com>
2015-09-06 05:57:11 -0400

    Fixed lint issue: E125 continuation line with same indent as next logical line.

--- iawscli/main.py
+++ iawscli/main.py
@@ -153,7 +153,7 @@ class IAwsCli(object):
     def colorize_output(self, text):
         if text.strip() != '' and \
             text.strip() != AWS_COMMAND[0] + ' ' + AWS_CONFIGURE[0] and \
-            AWS_COMMAND[0] in text.strip():
+                AWS_COMMAND[0] in text.strip():
             return text.strip() + ' | pygmentize -l json'
         else:
             return text
