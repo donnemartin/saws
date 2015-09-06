9f723fbd8b0939115f0dbe08db4d6f745f6e438f
Donne Martin <donne.martin@gmail.com>
2015-09-06 16:52:46 -0400

    Added shortcuts to lexer.

--- iawscli/lexer.py
+++ iawscli/lexer.py
@@ -4,9 +4,13 @@ from pygments.lexer import words
 from pygments.token import Keyword, Name, Operator, Generic, Literal
 from .commands import AWS_COMMAND, AWS_DOCS, CUSTOM_KEYWORDS, \
     generate_all_commands, CommandType
+from .config import read_configuration, get_shortcuts
 
 
 class CommandLexer(RegexLexer):
+
+    config = read_configuration()
+    shortcuts = get_shortcuts(config)
     commands = generate_all_commands()
     tokens = {
         'root': [
@@ -27,6 +31,10 @@ class CommandLexer(RegexLexer):
                    suffix=r'\b'),
              Generic.Output),
             (words(tuple(CUSTOM_KEYWORDS),
+                   prefix=r'',
+                   suffix=r'\b'),
+             Keyword.Declaration),
+            (words(tuple(shortcuts.keys()),
                    prefix=r'',
                    suffix=r'\b'),
              Keyword.Declaration),
