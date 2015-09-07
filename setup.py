ade5576c34e1ba8fcdf84e9e8d19b0768d69a0b5
Donne Martin <donne.martin@gmail.com>
2015-09-07 11:34:27 -0400

    Dropped Python 2.6 support. TODO: Needs some more work as OrderedDict is causing travis build errors.

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
         'configobj >= 5.0.6',
         'enum34>=1.0.4',
         'fuzzyfinder>=1.0.0',
-        'ordereddict>=1.1',
         'prompt-toolkit==0.46',
         'six>=1.9.0',
         'tabulate>=0.7.5',
