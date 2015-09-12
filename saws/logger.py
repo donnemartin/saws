6bf3fc3079043e4a3cc3163575b32538a2733995
Donne Martin <donne.martin@gmail.com>
2015-09-12 17:10:24 -0400

    Added docstrings for logger.py.

--- saws/logger.py
+++ saws/logger.py
@@ -5,30 +5,39 @@ import os
 import logging
 
 
-def create_logger(name, log_file, log_level):
+class SawsLogger(object):
+    """Handles Saws logging.
+
+    Attributes:
+        * logger: An instance of Logger.
     """
-    Create and return logger for package.
-    :param name: string logger name
-    :param log_file: string log file name
-    :param log_level: string
-    :return: logger
-    """
-    logger = logging.getLogger(name)
-    level_map = {
-        'CRITICAL': logging.CRITICAL,
-        'ERROR': logging.ERROR,
-        'WARNING': logging.WARNING,
-        'INFO': logging.INFO,
-        'DEBUG': logging.DEBUG
-    }
-    handler = logging.FileHandler(os.path.expanduser(log_file))
-    formatter = logging.Formatter(
-        '%(asctime)s (%(process)d/%(threadName)s) '
-        '%(name)s %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    root_logger = logging.getLogger('saws')
-    root_logger.addHandler(handler)
-    root_logger.setLevel(level_map[log_level.upper()])
-    root_logger.debug('Initializing saws logging.')
-    root_logger.debug('Log file %r.', log_file)
-    return logger
+
+    def __init__(self, name, log_file, log_level):
+        """Initializes a Logger for Saws.
+
+        Args:
+            * name: A string that represents the logger's name.
+            * log_file: A string that represents the log file name.
+            * log_level: A string that represents the logging level.
+
+        Returns:
+            None.
+        """
+        self.logger = logging.getLogger(name)
+        level_map = {
+            'CRITICAL': logging.CRITICAL,
+            'ERROR': logging.ERROR,
+            'WARNING': logging.WARNING,
+            'INFO': logging.INFO,
+            'DEBUG': logging.DEBUG
+        }
+        handler = logging.FileHandler(os.path.expanduser(log_file))
+        formatter = logging.Formatter(
+            '%(asctime)s (%(process)d/%(threadName)s) '
+            '%(name)s %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        root_logger = logging.getLogger('saws')
+        root_logger.addHandler(handler)
+        root_logger.setLevel(level_map[log_level.upper()])
+        root_logger.debug('Initializing saws logging.')
+        root_logger.debug('Log file %r.', log_file)
