bc37883e537b0c902cb969f20260025fa41632c9
Donne Martin <donne.martin@gmail.com>
2015-09-16 07:12:35 -0400

    Reworked test_refresh_forced unit test.

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -8,21 +8,24 @@ else:
     import unittest
 import mock
 from saws.saws import Saws
+from saws.resources import AwsResources
 
 
 class ResourcesTest(unittest.TestCase):
 
     def setUp(self):
-        self.create_resources()
         self.NUM_INSTANCE_IDS = 7
         self.NUM_INSTANCE_TAG_KEYS = 3
         self.NUM_INSTANCE_TAG_VALUES = 6
         self.NUM_BUCKET_NAMES = 16
+        self.RESOURCES = 'data/RESOURCES.txt'
+        self.RESOURCES_SAMPLE = 'data/RESOURCES_SAMPLE.txt'
+        self.create_resources()
 
     def create_resources(self):
         self.saws = Saws()
         self.resources = self.saws.completer.resources
-        self.resources.RESOURCE_FILE = 'data/RESOURCES_SAMPLE.txt'
+        self.resources.RESOURCE_FILE = self.RESOURCES_SAMPLE
 
     def test_refresh(self):
         self.resources.refresh(force_refresh=False)
@@ -35,17 +38,22 @@ class ResourcesTest(unittest.TestCase):
         assert len(self.resources.bucket_names) == \
             self.NUM_BUCKET_NAMES
 
-    @unittest.skip('TODO: Does not properly test a force refresh')
-    def test_refresh_forced(self):
-        self.resources.refresh(force_refresh=True)
-        assert len(self.resources.instance_ids) == \
-            self.NUM_INSTANCE_IDS
-        assert len(self.resources.instance_tag_keys) == \
-            self.NUM_INSTANCE_TAG_KEYS
-        assert len(self.resources.instance_tag_values) == \
-            self.NUM_INSTANCE_TAG_VALUES
-        assert len(self.resources.bucket_names) == \
-            self.NUM_BUCKET_NAMES
+    @mock.patch('saws.resources.subprocess')
+    def test_refresh_forced(self, mock_subprocess):
+        self.resources.RESOURCE_FILE = self.RESOURCES
+        with self.assertRaises(TypeError):
+            try:
+                self.resources.refresh(force_refresh=True)
+            except TypeError as e:
+                # The subprocess mock will cause this function to
+                # throw an exception.  Check the mock worked as
+                # expected before satisfying assertRaises.
+                mock_subprocess.check_output.assert_called_with(
+                    self.resources.QUERY_INSTANCE_IDS_CMD,
+                    universal_newlines=True,
+                    shell=True)
+                raise e
+        self.resources.RESOURCE_FILE = self.RESOURCES_SAMPLE
 
     @mock.patch('saws.resources.subprocess')
     def test_query_aws_with_instance_ids(self, mock_subprocess):
