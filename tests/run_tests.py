9da9e89e8cbac0fd0283b06bc9d42c4a7c8d406e
Donne Martin <donne.martin@gmail.com>
2015-09-17 07:03:32 -0400

    Added log_exception unit test for saws.py.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -6,6 +6,7 @@ import unittest
 from test_completer import CompleterTest  # NOQA
 from test_commands import CommandsTest  # NOQA
 from test_resources import ResourcesTest  # NOQA
+from test_saws import SawsTest  # NOQA
 from test_toolbar import ToolbarTest  # NOQA
 from test_keys import KeysTest  # NOQA
 try:
---
+++ tests/test_saws.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import print_function
+import mock
+import os
+import traceback
+import unittest
+from saws.saws import Saws
+from saws.commands import AwsCommands
+
+
+class SawsTest(unittest.TestCase):
+
+    @mock.patch('saws.resources.print')
+    def setUp(self, mock_print):
+        self.file_name = os.path.expanduser('~') + '/' + '.saws.log'
+        self.saws = Saws()
+        mock_print.assert_called_with('Loaded resources from cache')
+        self.DOCS_HOME_URL = 'http://docs.aws.amazon.com/cli/latest/reference/index.html'
+
+    @mock.patch('saws.saws.click')
+    def test_log_exception(self, mock_click):
+        exception_message = 'test_log_exception'
+        e = Exception(exception_message)
+        try:
+            raise e
+        except Exception:
+            # Traceback needs to have an active exception as described in:
+            # http://bugs.python.org/issue23003
+            self.saws.log_exception(e, traceback, echo=True)
+            mock_click.secho.assert_called_with(str(e), fg='red')
+        assert os.path.isfile(self.file_name)
+        with open(self.file_name, 'r') as fp:
+            for line in fp:
+                pass
+            assert exception_message in line
